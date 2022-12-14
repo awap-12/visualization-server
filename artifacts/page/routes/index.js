71b2de4a15941e912d6f6cda138e3bcae18b9b8d
深海里有坏龙龙 <seele.peng@gmail.com>
2022-12-14 21:36:59 +0200

    refactor(artifact): modified dependencies

--- artifacts/page/routes/index.js
+++ artifacts/page/routes/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
+const path = require("node:path");
+
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    let  data = {}
-
-    res.render("index", data)
+    res.sendFile(path.resolve(__dirname, "..", "views/index.html"));
 });
 
 module.exports = router;
